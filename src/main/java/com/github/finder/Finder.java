95bfb81424a719059a1b834f666607d0a4efb825
yuta39 <xoggkllo00@gmail.com>
2015-07-01 03:43:44 +0900

    refs #14 コマンドラインオプションで size オプションが指定された場合の処理を追加する
    修正完了しました。確認お願いします。

--- src/main/java/com/github/finder/Finder.java
+++ src/main/java/com/github/finder/Finder.java
@@ -7,22 +7,23 @@ import java.util.List;
 public class Finder {
     private Args args;
     
+
     public Finder(Args args){
         this.args = args;
     }
-    
+
     public String[] find(String target){
         List<String> list = new ArrayList<>();
-        
+
         traverse(list, new File(target));
-        
+
         return list.toArray(new String[list.size()]);
     }
-    
+
     private boolean isTarget(File file){
         return true;
     }
-    
+
     private void traverse(List<String> list, File dir){
         if(isTarget(dir)){
             list.add(dir.getPath());
