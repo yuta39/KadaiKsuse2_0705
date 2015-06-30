76e364cf57de66eed4ed468208f9aea103c9fc89
上野山雄太 <yu.uver.199469@gmail.com>
2015-07-01 03:35:59 +0900

    refs #13 コマンドラインオプションで type オプションが指定された場合の処理を追加する
    Finder.javaにtypeコマンドを追加しました。確認をお願いします。

--- src/main/java/com/github/finder/Finder.java
+++ src/main/java/com/github/finder/Finder.java
@@ -45,4 +45,27 @@ public class Finder {
        String name = file.getName();
        return name.indexOf(pattern) >= 0;
    }
+    private boolean isTarget(File file){
+        boolean flag = true;
+        if(args.getName() != null){
+            flag &= checkTargetName(file, args.getName());
+        }
+        if(args.getType() != null){
+            flag &= checkTargetType(file, args.getType());
+        }
+        return flag;
+    }
+    private boolean checkTargetType(File file, String type){
+        type = type.toLowerCase();
+        if(type.equals("d") || type.equals("directory")){
+            return file.isDirectory();
+        }
+        else if(type.equals("f") || type.equals("file")){
+            return file.isFile();
+        }
+        else if(type.equals("h") || type.equals("hidden")){
+            return file.isHidden();
+        }
+        return false;
+    }
 }
