d25498de18fe026d03ec7fa4e30feecb4cb4e596
yuta39 <xoggkllo00@gmail.com>
2015-07-01 04:10:40 +0900

    refs #22 コマンドラインオプションで size オプションが指定された場合の処理を追加する
    確認お願いします。

--- src/main/java/com/github/finder/Finder.java
+++ src/main/java/com/github/finder/Finder.java
@@ -1,6 +1,5 @@
 package com.github.finder;
 
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,9 +19,6 @@ public class Finder {
         return list.toArray(new String[list.size()]);
     }
 
-    private boolean isTarget(File file){
-        return true;
-    }
 
     private void traverse(List<String> list, File dir){
         if(isTarget(dir)){
@@ -33,5 +29,84 @@ public class Finder {
                 traverse(list, file);
             }
         }
+    }
+
+   private boolean checkTargetName(File file, String pattern){
+       String name = file.getName();
+       return name.indexOf(pattern) >= 0;
+   }
+    private boolean isTarget(File file){
+        boolean flag = true;
+        if(args.getName() != null){
+            flag &= checkTargetName(file, args.getName());
+        }
+        if(args.getType() != null){
+            flag &= checkTargetType(file, args.getType());
+        }
+        return flag;
+    }
+    private boolean checkTargetType(File file, String type){
+        type = type.toLowerCase();
+        if(type.equals("d") || type.equals("directory")){
+            return file.isDirectory();
+        }
+        else if(type.equals("f") || type.equals("file")){
+            return file.isFile();
+        }
+        else if(type.equals("h") || type.equals("hidden")){
+            return file.isHidden();
+        }
+        return false;
+    }
+    private boolean isTarget(File file){
+        boolean flag = true;
+        if(args.getName() != null){
+            flag &= checkTargetName(file, args.getName());
+        }
+        if(args.getType() != null){
+            flag &= checkTargetType(file, args.getType());
+        }
+        if(args.getSize() != null){
+            flag &= checkTargetSize(file, args.getSize());
+        }
+        if(args.getGrep() != null){
+            flag &= checkGrep(file, args.getGrep());
+        }
+
+        return flag;
+    }
+
+    private boolean checkTargetSize(File file, String sizeString){
+        if(file.isFile()){
+            char sign = sizeString.charAt(0);
+            String string = sizeString.substring(1);
+            int size = Integer.parseInt(string);
+
+            switch(sign){
+            case '>':
+                return file.length() > size;
+            case '<':
+                return file.length() < size;
+            case '=':
+                return file.length() == size;
+            default:
+                // ignore
+            }
+        }
+        return false;
+    }
+
+    private boolean checkGrep(File file, String pattern){
+        if(file.isFile()){
+            try(BufferedReader in = new BufferedReader(new FileReader(file))){
+                String line;
+                while((line = in.readLine()) != null){
+                    if(line.indexOf(pattern) >= 0){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
